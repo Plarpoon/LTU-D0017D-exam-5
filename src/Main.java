8a3dc1f6c4329207ab0065b035807100f9aa0ace
Alex <plarpoon.dev@gmail.com>
2022-11-26 04:50:17 +0100

    completed part 1, be aware math is wrong.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
     }
 
     private static void SETUP_EX1(Scanner scanner) {
-        CLEAR_CONSOLE();
         PRINT_MENU_EX1();
         EXEC_EX1(scanner);
     }
@@ -24,36 +23,54 @@ public class Main {
         // Read input into string array
         String[] input = scanner.nextLine().split(" ");
 
-        // DEBUG: Print whole array
-        for (String s : input) {
-            System.out.println(s);
+        // While loop until array is empty
+        while (input.length > 0) {
+            // If 'q' or 'Q' is entered, proceed to next exercise
+            if (input[0].equals("q") || input[0].equals("Q")) {
+                System.out.println("DEBUG: NEXT EXCERCISE LOADING...");
+                System.exit(0);
+            }
+
+            // Assign input[0] to radius
+            int radius = Integer.parseInt(input[0]);
+
+            // Assign input[1] to height
+            int height = Integer.parseInt(input[1]);
+
+            // Print results
+            PRINT_RESULTS_EX1(radius, height);
+
+            // Remove first element from array
+            if (input.length >= 2) {
+                input = REMOVE_FIRST_TWO_ELEMENTS(input);
+            }
+        }
+    }
+
+    private static void PRINT_RESULTS_EX1(int radius, int height) {
+        // Print results to console with rounding
+        System.out.println("r = " + radius + " h = " + height);
+        System.out.println("Area: " + String.format("%.2f", AREA(radius)));
+        System.out.println("Surface area: " + String.format("%.2f", AREA(radius, height)));
+        System.out.println("Volume: " + String.format("%.2f", VOLUME(radius, height)));
+        System.out.println("\n");
+    }
+
+    private static String[] REMOVE_FIRST_TWO_ELEMENTS(String[] input) {
+        // Create new array with length - 2
+        String[] newArray = new String[input.length - 2];
+
+        // Copy all elements from input to newArray
+        for (int i = 2; i < input.length; i++) {
+            newArray[i - 2] = input[i];
         }
 
-        // If 'q' or 'Q' is entered, proceed to next exercise
-        if (input[0].equals("q") || input[0].equals("Q")) {
-            System.out.println("DEBUG: NEXT EXCERCISE LOADING...");
-        }
-
-        // Assign input[0] to radius
-        int radius = Integer.parseInt(input[0]);
-
-        // Calculate area
-        AREA(radius);
-
-        // Assign input[1] to height
-        int height = Integer.parseInt(input[1]);
-
-        // Calculate surgaface area
-        AREA(radius, height);
-
-        // Calculate volume
-        VOLUME(radius, height);
+        return newArray;
     }
 
     private static double VOLUME(int radius, int height) {
         // Calculate volume of the cone
         double volume = Math.PI * Math.sqrt(radius) * Math.sqrt(height) / 3;
-        System.out.println("Volume: " + volume);
 
         return volume;
     }
@@ -68,7 +85,6 @@ public class Main {
     private static double AREA(int radius, int height) {
         // Calculate area of the tip of the cone
         double surface_area = Math.PI * radius * SLANT_HEIGHT(radius, height);
-        System.out.println("Surface area: " + surface_area);
 
         return surface_area;
     }
@@ -76,7 +92,6 @@ public class Main {
     private static double AREA(int radius) {
         // Calculate base surface area (area of the circle)
         double base_area = Math.PI * Math.sqrt(radius);
-        System.out.println("Base area: " + base_area);
 
         return base_area;
     }
