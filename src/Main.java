d59a1b89f509f8bddfae92ffe239509726b8eac7
Alex <plarpoon.dev@gmail.com>
2022-12-03 02:29:13 +0100

    rewrote entire project with explaination from prof in mind

--- src/Main.java
+++ src/Main.java
@@ -1,109 +1,77 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        // Add scanner
-        Scanner scanner = new Scanner(System.in);
+        final int PROCEED = -1;
+        final String TOO_SMALL = "Value must be greater than 0. Please try again.";
 
-        SETUP_EX1(scanner);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        PRINT_MENU_1();
+        READ_INPUT_1(PROCEED, TOO_SMALL, numbers);
+        CALCULATE_PHASE_1(numbers);
     }
 
-    private static void SETUP_EX1(Scanner scanner) {
-        PRINT_MENU_EX1();
-        EXEC_EX1(scanner);
+    private static void CALCULATE_PHASE_1(ArrayList<Integer> numbers) {
+        System.out.println("DEBUG: CALCULATING PHASE 1");
+
+        // Print all numbers for debugging
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 
-    private static void CLEAR_CONSOLE() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+    private static ArrayList<Integer> READ_INPUT_1(final int PROCEED, final String TOO_SMALL,
+            ArrayList<Integer> numbers) {
+        while (true) {
+            int value = 0;
+
+            value = INPUT(PROCEED);
+
+            // TODO: Move this to execution phase when it read this value in the array
+            if (value == PROCEED) {
+                System.out.println("DEBUG: PROCEEDING TO PART TWO");
+                break;
+            }
+
+            if (value < 0) {
+                System.out.println(TOO_SMALL);
+                System.out.print("> ");
+                continue;
+            }
+
+            numbers.add(value);
+        }
+        return (numbers);
     }
 
-    private static void EXEC_EX1(Scanner scanner) {
+    private static int INPUT(final int PROCEED) {
+        String in;
+        final String LINE_MARKER = "> ";
 
-        while (scanner.hasNext()) {
-            // Read input into string array
-            String[] input = scanner.nextLine().split(" ");
-
-            // While loop until array is empty
-            while (input.length > 0) {
-                // If 'q' or 'Q' is entered, proceed to next exercise
-                if (input[0].equals("q") || input[0].equals("Q")) {
-                    System.out.println("DEBUG: NEXT EXCERCISE LOADING...");
-                    CLEAR_CONSOLE();
-                    System.exit(0);
-                }
-
-                // Assign input[0] to radius
-                int radius = Integer.parseInt(input[0]);
-
-                // Assign input[1] to height
-                int height = 0;
-                if (input.length > 1) {
-                    height = Integer.parseInt(input[1]);
-                }
-
-                // Print results
-                PRINT_RESULTS_EX1(radius, height);
-
-                // Remove first element from array
-                if (input.length >= 2) {
-                    input = REMOVE_FIRST_TWO_ELEMENTS(input);
+        while (true) {
+            // Check if input is an integer
+            if (input.hasNextInt()) {
+                return input.nextInt();
+            } else {
+                // Check if input is equal to 'q'
+                in = input.next();
+                if (in.equals("q")) {
+                    return PROCEED;
+                } else {
+                    System.out.println("Invalid input. Please try again.");
+                    System.out.print(LINE_MARKER);
                 }
             }
         }
     }
 
-    private static void PRINT_RESULTS_EX1(int radius, int height) {
-        // Print results to console with rounding
-        System.out.println("r = " + radius + " h = " + height);
-        System.out.println("Area: " + String.format("%.2f", AREA(radius)));
-        System.out.println("Surface area: " + String.format("%.2f", AREA(radius, height)));
-        System.out.println("Volume: " + String.format("%.2f", VOLUME(radius, height)));
-        System.out.println("\n");
-    }
-
-    private static String[] REMOVE_FIRST_TWO_ELEMENTS(String[] input) {
-        // Create new array with length - 2
-        String[] newArray = new String[input.length - 2];
-
-        // Copy all elements from input to newArray
-        for (int i = 2; i < input.length; i++) {
-            newArray[i - 2] = input[i];
-        }
-
-        return newArray;
-    }
-
-    private static double VOLUME(int radius, int height) {
-        // Calculate volume of the cone
-        double volume = Math.PI * Math.sqrt(radius) * Math.sqrt(height) / 3;
-
-        return volume;
-    }
-
-    private static double SLANT_HEIGHT(int radius, int height) {
-        // Calculate slant height of the cone
-        double slant_height = Math.sqrt(radius) + Math.sqrt(height);
-
-        return slant_height;
-    }
-
-    private static double AREA(int radius, int height) {
-        // Calculate area of the tip of the cone
-        double surface_area = Math.PI * radius * SLANT_HEIGHT(radius, height);
-
-        return surface_area;
-    }
-
-    private static double AREA(int radius) {
-        // Calculate base surface area (area of the circle)
-        double base_area = Math.PI * Math.sqrt(radius);
-
-        return base_area;
-    }
-
-    private static void PRINT_MENU_EX1() {
+    private static void PRINT_MENU_1() {
         System.out.println("---------------------------------");
         System.out.println("# Test of area and volume methods");
         System.out.println("---------------------------------");
